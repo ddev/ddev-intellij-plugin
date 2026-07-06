e80e5edb4b11fc28194a32f4efc00fe96ea9b637
Akiba <dev@akiba.at>
2026-07-06 14:04:31 +0200

    feat: add Xdebug enable and disable actions

---
+++ src/main/java/de/php_perfect/intellij/ddev/actions/DdevDisableXdebugAction.java
@@ -0,0 +1,12 @@
+package de.php_perfect.intellij.ddev.actions;
+
+import com.intellij.openapi.project.Project;
+import de.php_perfect.intellij.ddev.cmd.DdevRunner;
+import org.jetbrains.annotations.NotNull;
+
+public final class DdevDisableXdebugAction extends DdevRunningAction {
+    @Override
+    protected void run(@NotNull Project project) {
+        DdevRunner.getInstance().disableXdebug(project);
+    }
+}
---
+++ src/main/java/de/php_perfect/intellij/ddev/actions/DdevEnableXdebugAction.java
@@ -0,0 +1,12 @@
+package de.php_perfect.intellij.ddev.actions;
+
+import com.intellij.openapi.project.Project;
+import de.php_perfect.intellij.ddev.cmd.DdevRunner;
+import org.jetbrains.annotations.NotNull;
+
+public final class DdevEnableXdebugAction extends DdevRunningAction {
+    @Override
+    protected void run(@NotNull Project project) {
+        DdevRunner.getInstance().enableXdebug(project);
+    }
+}
