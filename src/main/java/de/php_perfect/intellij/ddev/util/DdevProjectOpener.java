7e18932154bf55f89ba2d8cc7419c94e07033735
Akiba <dev@akiba.at>
2026-09-06 16:40:47 +0200

    fix: replace deprecated IntelliJ APIs

--- src/main/java/de/php_perfect/intellij/ddev/util/DdevProjectOpener.java
+++ src/main/java/de/php_perfect/intellij/ddev/util/DdevProjectOpener.java
@@ -1,7 +1,8 @@
 package de.php_perfect.intellij.ddev.util;
 
-import com.intellij.ide.impl.OpenProjectTask;
+import com.intellij.ide.impl.OpenProjectTaskBuilder;
 import com.intellij.ide.impl.ProjectUtil;
+import kotlin.Unit;
 import org.jetbrains.annotations.NotNull;
 
 import java.nio.file.Path;
@@ -14,10 +15,16 @@ public final class DdevProjectOpener {
     }
 
     public static void openInCurrentWindow(@NotNull String path) {
-        ProjectUtil.openOrImport(Path.of(path), OpenProjectTask.build().withForceOpenInNewFrame(false));
+        ProjectUtil.openOrImport(Path.of(path), new OpenProjectTaskBuilder().build(builder -> {
+            builder.setForceOpenInNewFrame(false);
+            return Unit.INSTANCE;
+        }));
     }
 
     public static void openInNewWindow(@NotNull String path) {
-        ProjectUtil.openOrImport(Path.of(path), OpenProjectTask.build().withForceOpenInNewFrame(true));
+        ProjectUtil.openOrImport(Path.of(path), new OpenProjectTaskBuilder().build(builder -> {
+            builder.setForceOpenInNewFrame(true);
+            return Unit.INSTANCE;
+        }));
     }
 }
